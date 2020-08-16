93a31df8e1e66903c1f8772f4c4d90c7432adc2c
yondo123 <yondo123@gmail.com>
2020-08-17 00:40:58 +0900

    2019카카오코딩테스트_인형뽑기

--- JavaScript/04_Algorithm/kakao2019_crane.js
+++ JavaScript/04_Algorithm/kakao2019_crane.js
@@ -1,20 +1,29 @@
-var board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]];
-var moves = [1,5,3,5,1,2,1,4];
+var board = [[1,1,1,1,1],[0,0,0,1,3],[0,0,5,1,1],[4,4,4,4,1],[3,5,1,1,1]];
+var moves = [1,4,3,5,1,2,1,4];
 
 function solution(board, moves) {
     var answer = 0;
     var resArray = [];
+    var latestItem = 0;
     for (let i = 0; i < moves.length; i++) {
         if(board[moves[i]-1][board[moves[i]-1].length-1] > 0){
             resArray.push(board[moves[i]-1][board[moves[i]-1].length-1]);
         }
         board[moves[i]-1].pop();
     }
-    
+
+    console.log(resArray);
+
     for(let j = 0; j < resArray.length; j++){
-        if(resArray[j] === resArray[j+1]){
-            answer+=2;
+        console.log(j);
+        if(!resArray[j]){
+            continue;
         }
+        if(resArray[j] === latestItem && j > 0){
+            answer+=2;
+            resArray.splice(j,1);
+        }
+        latestItem = resArray[j];
     }
     console.log(answer);
     return answer;
