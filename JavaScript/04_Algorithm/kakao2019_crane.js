e2fdf9665ea255d02678503b899aeb61fa91e889
yondo123 <yondo123@gmail.com>
2020-08-17 17:13:32 +0900

    2019_카카오코딩테스트 인턴쉽_크레인 인형뽑기 > 최종 제출

--- JavaScript/04_Algorithm/kakao2019_crane.js
+++ JavaScript/04_Algorithm/kakao2019_crane.js
@@ -1,32 +1,39 @@
-var board = [[1,1,1,1,1],[0,0,0,1,3],[0,0,5,1,1],[4,4,4,4,1],[3,5,1,1,1]];
-var moves = [1,4,3,5,1,2,1,4];
+/*
+  URL : https://programmers.co.kr/learn/courses/30/lessons/64061?language=javascript 
+*/
 
+var board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]; //테스트케이스 - 인형뽑기 박스
+var moves = [1,5,3,5,1,2,1,4]; //테스트케이스 - 크레인 움직임
+
+//배열 비교함수
+function itemCompare(arr, cnt){
+    let count = cnt ? cnt : 0; //누적된 cnt가 없다면 0으로 초기화
+    for(let i=0; i<arr.length; i++){
+        if(!arr[i]) continue;
+        if(arr[i]==arr[i+1]){ //양 옆 요소가 같다면 제거 후, 다시 비교
+            count+=2;
+            arr.splice(i, 2);    
+            return itemCompare(arr, count);
+        }
+    }
+    return count;
+}
+
+//풀이 함수
 function solution(board, moves) {
-    var answer = 0;
-    var resArray = [];
-    var latestItem = 0;
+    let resArray = []; //뽑기 결과 배열
     for (let i = 0; i < moves.length; i++) {
-        if(board[moves[i]-1][board[moves[i]-1].length-1] > 0){
-            resArray.push(board[moves[i]-1][board[moves[i]-1].length-1]);
+        let targetPos = moves[i]-1;
+        for(let j = 0; j < board[moves[i]-1].length; j++){
+            const item = board[j][targetPos];
+            if(item > 0){ 
+                resArray.push(item);
+                board[j][targetPos] = 0;
+                break;
+            }
         }
-        board[moves[i]-1].pop();
     }
-
-    console.log(resArray);
-
-    for(let j = 0; j < resArray.length; j++){
-        console.log(j);
-        if(!resArray[j]){
-            continue;
-        }
-        if(resArray[j] === latestItem && j > 0){
-            answer+=2;
-            resArray.splice(j,1);
-        }
-        latestItem = resArray[j];
-    }
-    console.log(answer);
-    return answer;
+    return itemCompare(resArray)
 }
 
 solution(board, moves);
